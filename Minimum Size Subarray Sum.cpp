cdeb5f863b0abc799e6d1f27b8b5df505109c1e2
Keshav Goel <70531228+keshav25201@users.noreply.github.com>
2022-01-06 17:12:11 +0530

    Create Minimum Size Subarray Sum.cpp

---
+++ Minimum Size Subarray Sum.cpp
@@ -0,0 +1,17 @@
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //lets try two pointer method
+        int n = nums.size();
+        int ans = -1;
+        int sum = 0;
+        for(int i = 0,j = 0; i < n;i++)
+        {
+            sum+=nums[i];
+            while(sum >= target && j <= i)
+            {
+                if(ans == -1 || i-j+1 < ans)ans = i-j+1;
+                sum-=nums[j];
+                j++;
+            }
+        }
+        return ans==-1?0:ans;
+    }
